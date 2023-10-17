a33495766f9604c7220ae4a2d72349a935812a5d
Cesar-Moran <cesarmorangithub@gmail.com>
2023-10-16 23:03:03 -0500

    FIRST COMMIT

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      white: '#ffffff',
+      lightgrey: '#d6e2f0', 
+      gBlue: '#7b879d', 
+      dBlue: '#1f3251', 
+    },
+    fontFamily: {
+      outfit: ['Outfit', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
